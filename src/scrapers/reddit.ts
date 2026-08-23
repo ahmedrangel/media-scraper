95d765d7df91d3c914abcc2177a191d016c50764
Ahmed Rangel <ahmedrangel@outlook.com>
2026-08-23 13:28:05 -0500

    fix(reddit): use author icon_img as avatar url and use snoovatar_img as fallback

--- src/scrapers/reddit.ts
+++ src/scrapers/reddit.ts
@@ -119,7 +119,7 @@ export default async (url: string): Promise<RedditMedia> => {
       id: data?.author_fullname?.replace("t2_", ""),
       name: data?.author,
       username: data?.author,
-      avatar_url: authorData?.data?.snoovatar_img,
+      avatar_url: authorData?.data?.icon_img?.replace(/&amp;/g, "&") || authorData?.data?.snoovatar_img,
       url: data?.author ? `https://www.reddit.com/user/${data?.author}/` : undefined
     },
     up_count: data?.ups,
